411c291e88020ed67584f2687d63b188ebe14b7b
gustavo <gustavoneri22@gmail.com>
2023-11-14 22:37:57 -0300

    main

---
+++ bin/main.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+
+import 'package:arvore_binaria/arvore_binaria.dart';
+import 'package:arvore_binaria/no_arvore.dart';
+
+void main(List<String> arguments) {
+  List<int> valoresInseridos = [];
+
+  final int valorRaiz = Random().nextInt(101);
+  print('Valor do Nó Raiz: $valorRaiz');
+  ArvoreBinaria arvoreBinaria = ArvoreBinaria(raiz: NoArvore(valorRaiz));
+
+  // Sorteio de 20 números e inserção na árvore
+  for (int i = 0; i < 19; i++) {
+    int randomNumber = Random().nextInt(101);
+    print('Nó inserido: $randomNumber');
+    arvoreBinaria.inserirNo(arvoreBinaria.raiz, randomNumber);
+    valoresInseridos.add(randomNumber);
+  }
+
+  // arvoreBinaria.retira(arvoreBinaria.raiz, 15);
+
+  print("\n\nEM ORDEM:\n");
+  arvoreBinaria.imprimirEmOrdem(arvoreBinaria.raiz);
+  print("\n\nPRÉ ORDEM:\n");
+  arvoreBinaria.imprimirPreOrdem(arvoreBinaria.raiz);
+  print("\n\nPÓS ORDEM:\n");
+  arvoreBinaria.imprimirPosOrdem(arvoreBinaria.raiz);
+  print("\n\nEM NÍVEL:\n");
+  arvoreBinaria.imprimirEmNivel();
+
+  print('------------------- REMOÇÃO -------------------');
+
+  // Sorteio de 20 números e remoção na árvore
+  for (int i = 0; i < 5; i++) {
+    int? randomNumber;
+    while (!valoresInseridos.contains(randomNumber)) {
+      randomNumber = Random().nextInt(101);
+      print('Nó a ser removido: $randomNumber');
+      arvoreBinaria.removerNo(arvoreBinaria.raiz, randomNumber);
+    }
+  }
+
+  print("\n\nEM ORDEM:\n");
+  arvoreBinaria.imprimirEmOrdem(arvoreBinaria.raiz);
+  print("\n\nPRÉ ORDEM:\n");
+  arvoreBinaria.imprimirPreOrdem(arvoreBinaria.raiz);
+  print("\n\nPÓS ORDEM:\n");
+  arvoreBinaria.imprimirPosOrdem(arvoreBinaria.raiz);
+  print("\n\nEM NÍVEL:\n");
+  arvoreBinaria.imprimirEmNivel();
+}
